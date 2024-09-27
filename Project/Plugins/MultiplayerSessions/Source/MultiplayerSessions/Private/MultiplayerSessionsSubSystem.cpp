bdd6202f70e4fedd7dd1b27a37e118a9d23f3db8
ivanovmandevivan <ivanovmandevivan@gmail.com>
2024-09-27 16:06:42 +0100

    Setup Methods and Comments for MultiplayerSessionsSubSystem
    - Setup main public API methods, delegates signatures, handles and delegate methods for the class.

--- Project/Plugins/MultiplayerSessions/Source/MultiplayerSessions/Private/MultiplayerSessionsSubSystem.cpp
+++ Project/Plugins/MultiplayerSessions/Source/MultiplayerSessions/Private/MultiplayerSessionsSubSystem.cpp
@@ -3,11 +3,56 @@
 // Online -> OnlineSubsystem
 #include "OnlineSubsystem.h"
 
-UMultiplayerSessionsSubSystem::UMultiplayerSessionsSubSystem(const FObjectInitializer& ObjectInitializer)
+UMultiplayerSessionsSubSystem::UMultiplayerSessionsSubSystem(const FObjectInitializer& ObjectInitializer) :
+    CreateSessionCompleteDelegate(FOnCreateSessionCompleteDelegate::CreateUObject(this, &ThisClass::OnCreateSessionComplete)),
+    FindSessionsCompleteDelegate(FOnFindSessionsCompleteDelegate::CreateUObject(this, &ThisClass::OnFindSessionsComplete)),
+    JoinSessionCompleteDelegate(FOnJoinSessionCompleteDelegate::CreateUObject(this, &ThisClass::OnJoinSessionComplete)),
+    DestroySessionCompleteDelegate(FOnDestroySessionCompleteDelegate::CreateUObject(this, &ThisClass::OnDestroySessionComplete)),
+    StartSessionCompleteDelegate(FOnStartSessionCompleteDelegate::CreateUObject(this, &ThisClass::OnStartSessionComplete))
 {
     IOnlineSubsystem* Subsystem = IOnlineSubsystem::Get();
     check(Subsystem); // Something very wrong has happened if the OnlineSubsystem does not return a valid ptr.
 
     SessionInterface = Subsystem->GetSessionInterface();
     ensureAlways(nullptr != SessionInterface);
+}
+
+void UMultiplayerSessionsSubSystem::CreateSession(int32 NumPublicConnections, const FString& MatchType)
+{
+}
+
+void UMultiplayerSessionsSubSystem::FindSessions(int32 MaxSearchResults)
+{
+}
+
+void UMultiplayerSessionsSubSystem::JoinSession(const FOnlineSessionSearchResult& SessionResult)
+{
+}
+
+void UMultiplayerSessionsSubSystem::DestroySession()
+{
+}
+
+void UMultiplayerSessionsSubSystem::StartSession()
+{
+}
+
+void UMultiplayerSessionsSubSystem::OnCreateSessionComplete(FName SessionName, bool bWasSuccessful)
+{
+}
+
+void UMultiplayerSessionsSubSystem::OnFindSessionsComplete(bool bWasSuccessful)
+{
+}
+
+void UMultiplayerSessionsSubSystem::OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result)
+{
+}
+
+void UMultiplayerSessionsSubSystem::OnDestroySessionComplete(FName SessionName, bool bWasSuccessful)
+{
+}
+
+void UMultiplayerSessionsSubSystem::OnStartSessionComplete(FName SessionName, bool bWasSuccessful)
+{
 }
--- Project/Plugins/MultiplayerSessions/Source/MultiplayerSessions/Public/MultiplayerSessionsSubSystem.h
+++ Project/Plugins/MultiplayerSessions/Source/MultiplayerSessions/Public/MultiplayerSessionsSubSystem.h
@@ -19,7 +19,123 @@ class MULTIPLAYERSESSIONS_API UMultiplayerSessionsSubSystem : public UGameInstan
 	
 public:
 		UMultiplayerSessionsSubSystem(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+		virtual ~UMultiplayerSessionsSubSystem() = default;
+
+		/**
+		 * These methods are going to be used by the UI/BP to be able to execute the functionality in regards to Session code.
+		 */
+
+		/**
+		 * Method binded to the 'CreateSessionCompleteDelegate' delegate to handle creation of sessions. 
+		 */
+		void CreateSession(int32 NumPublicConnections, const FString& MatchType);
+		
+		/**
+		 * Method binded to the 'FindSessionsCompleteDelegate' delegate to find sessions.
+		 */
+		void FindSessions(int32 MaxSearchResults);
+		
+		/**
+		 * Method binded to the 'JoinSessionCompleteDelegate' delegate to join sessions.
+		 */
+		void JoinSession(const FOnlineSessionSearchResult& SessionResult);
+		
+		/**
+		 * Method binded to the 'DestroySessionCompleteDelegate' delegate to destroy sessions.
+		 */
+		void DestroySession();
+		
+		/**
+		 * Method binded to the 'StartSessionCompleteDelegate' delegate to start sessions.
+		 */
+		void StartSession();
+
+protected:
+		
+		/**
+		 * Internal callback for the delegates that will be added to the Online Session Interface Delegate List.
+		 * Called whenever the creation of a session is completed.
+		 */
+		void OnCreateSessionComplete(FName SessionName, bool bWasSuccessful);
+
+		/**
+		 * Internal callback for the delegates that will be added to the Online Session Interface Delegate List.
+		 * Called whenever a find query for a session is completed.
+		 */
+		void OnFindSessionsComplete(bool bWasSuccessful);
+
+		/**
+		 * Internal callback for the delegates that will be added to the Online Session Interface Delegate List.
+		 * Called whenever the join of a session is completed.
+		 */
+		void OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result);
+
+		/**
+     * Internal callback for the delegates that will be added to the Online Session Interface Delegate List.
+     * Called whenever a session destroy is completed.
+     */
+		void OnDestroySessionComplete(FName SessionName, bool bWasSuccessful);
+
+		/**
+		 * Internal callback for the delegates that will be added to the Online Session Interface Delegate List.
+		 * Called whenever the start of a session is completed.
+		 */
+		void OnStartSessionComplete(FName SessionName, bool bWasSuccessful);
 
 private:
+		/**
+		 * Pointer to a TSharedPtr<class IOnlineSession>
+		 * Main bridge class with which to create, destroy, find, start and stop sessions.
+		 */
 		IOnlineSessionPtr SessionInterface;
+
+		/**
+		 * Delegate fired when a session create request has completed.
+		 */
+		FOnCreateSessionCompleteDelegate CreateSessionCompleteDelegate;
+
+		/**
+		* Delegate handle for the 'CreateSessionCompleteDelegate' event.
+		*/
+		FDelegateHandle CreateSessionCompleteDelegateHandle;
+
+		/**
+		 * Delegate fired when the search for an online session has completed.
+		 */
+		FOnFindSessionsCompleteDelegate FindSessionsCompleteDelegate;
+
+		/**
+		* Delegate handle for the 'FindSessionsCompleteDelegate' event.
+		*/
+		FDelegateHandle FindSessionsCompleteDelegateHandle;
+
+		/**
+		 * Delegate fired when the joining process for an online session has completed.
+		 */
+		FOnJoinSessionCompleteDelegate JoinSessionCompleteDelegate;
+
+		/**
+		* Delegate handle for the 'JoinSessionCompleteDelegate' event.
+		*/
+		FDelegateHandle JoinSessionCompleteDelegateHandle;
+
+		/**
+		 * Delegate fired when a destroying an online session has completed.
+		 */
+		FOnDestroySessionCompleteDelegate DestroySessionCompleteDelegate;
+
+		/**
+		* Delegate handle for the 'DestroySessionCompleteDelegate' event.
+		*/
+		FDelegateHandle DestroySessionCompleteDelegateHandle;
+
+		/**
+		 * Delegate fired when the online session has transitioned to the started state.
+		 */
+		FOnStartSessionCompleteDelegate StartSessionCompleteDelegate;
+
+		/**
+		* Delegate handle for the 'StartSessionCompleteDelegate' event.
+		*/
+		FDelegateHandle StartSessionCompleteDelegateHandle;
 };
