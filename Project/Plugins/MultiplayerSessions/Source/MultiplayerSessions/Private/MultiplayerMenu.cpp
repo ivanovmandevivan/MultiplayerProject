20b60d0da83c3087db6909759b4f5e60e49cdc18
ivanovmandevivan <ivanovmandevivan@gmail.com>
2024-09-27 18:35:09 +0100

    MultiplayerSessionSubsystem Get() & Host/Join Buttons printing on screen
    - Host button prints on screen and calls on HostSession(), hardcoded to 4 players, Free For All.
    - Join button just prints on screen for now.

--- Project/Plugins/MultiplayerSessions/Source/MultiplayerSessions/Private/MultiplayerMenu.cpp
+++ Project/Plugins/MultiplayerSessions/Source/MultiplayerSessions/Private/MultiplayerMenu.cpp
@@ -1,10 +1,16 @@
 #include "MultiplayerMenu.h"
 
+// Runtime -> UMG
+#include "Components/Button.h"
+
+// MultiplayerSessions -> MultiplayerSessions
+#include "MultiplayerSessionsSubSystem.h"
+
 void UMultiplayerMenu::SetupMenu()
 {
     AddToViewport();
     SetVisibility(ESlateVisibility::Visible);
-    bIsFocusable = true;
+    SetIsFocusable(true);
 
     UWorld* World = GetWorld();
     if (!ensureAlwaysMsgf(nullptr != World, TEXT("Unable to fetch a valid world for the Multiplayer Menu Widget.")))
@@ -23,4 +29,60 @@ void UMultiplayerMenu::SetupMenu()
     InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
 
     PlayerController->SetInputMode(InputModeData);
+
+    // Get Multiplayer Session Subsystem:
+    UGameInstance* GameInstance = GetGameInstance();
+    if (!ensureAlwaysMsgf(nullptr != GameInstance, TEXT("Unable to get game instance, therefore can't fetch MultiplayerSessionSubSystem.")))
+    {
+        return;
+    }
+
+    MultiplayerSessionsSubsystem = GameInstance->GetSubsystem<UMultiplayerSessionsSubSystem>();
+}
+
+bool UMultiplayerMenu::Initialize()
+{
+    if (!Super::Initialize())
+    {
+        return false;
+    }
+
+    if (!HostButton || !JoinButton)
+    {
+        return false;
+    }
+
+    HostButton->OnClicked.AddDynamic(this, &ThisClass::HostButtonClicked);
+    JoinButton->OnClicked.AddDynamic(this, &ThisClass::JoinButtonClicked);
+
+    return true;
+}
+
+void UMultiplayerMenu::HostButtonClicked()
+{
+    if (GEngine)
+    {
+        GEngine->AddOnScreenDebugMessage(
+            -1,
+            15.f,
+            FColor::Yellow,
+            FString(TEXT("Host Button Clicked!"))
+        );
+    }
+
+    ensureAlways(MultiplayerSessionsSubsystem);
+    MultiplayerSessionsSubsystem->CreateSession(4, FString("FreeForAll"));
+}
+
+void UMultiplayerMenu::JoinButtonClicked()
+{
+    if (GEngine)
+    {
+        GEngine->AddOnScreenDebugMessage(
+            -1,
+            15.f,
+            FColor::Yellow,
+            FString(TEXT("Join Button Clicked!"))
+        );
+    }
 }
--- Project/Plugins/MultiplayerSessions/Source/MultiplayerSessions/Public/MultiplayerMenu.h
+++ Project/Plugins/MultiplayerSessions/Source/MultiplayerSessions/Public/MultiplayerMenu.h
@@ -6,8 +6,12 @@
 // Runtime -> UMG
 #include "Blueprint/UserWidget.h"
 
+// MultiplayerSessions -> MultiplayerSessions
+class UMultiplayerSessionsSubSystem;
+
 // Generated
 #include "MultiplayerMenu.generated.h"
+
 
 UCLASS()
 class MULTIPLAYERSESSIONS_API UMultiplayerMenu : public UUserWidget
@@ -18,4 +22,27 @@ public:
 
 		UFUNCTION(BlueprintCallable, Category="MultiplayerMenu|Functions")
 		void SetupMenu();
+
+protected:
+
+		virtual bool Initialize() override;
+
+private:
+
+		UFUNCTION()
+		void HostButtonClicked();
+		
+		UFUNCTION()
+		void JoinButtonClicked();
+
+		UPROPERTY(meta=(BindWidget))
+		class UButton* HostButton;
+
+		UPROPERTY(meta=(BindWidget))
+		class UButton* JoinButton;
+
+		/**
+		 * Subsystem designed to handle all online session functionality (e.g: start, stop, create, destroy, find, etc).
+		 */
+		TObjectPtr<UMultiplayerSessionsSubSystem> MultiplayerSessionsSubsystem;
 };
